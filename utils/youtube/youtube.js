0d1612e5ed51177474a91b2f52fd59567d71b06e
salvo-github <q4zsktlr@unglazed6993.anonaddy.com>
2025-09-19 22:20:18 +0200

    Rename channel.js to channelInfo.js for better clarity
    - More descriptive filename for channel identifier resolution logic
    - Update import path in youtube.js

--- utils/youtube/youtube.js
+++ utils/youtube/youtube.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 import logger from '../logger.js';
-import { getChannelUUID } from './channel.js';
+import { getChannelUUID } from './channelInfo.js';
 
 const YOUTUBE_API_BASE = 'https://www.googleapis.com/youtube/v3';
 
